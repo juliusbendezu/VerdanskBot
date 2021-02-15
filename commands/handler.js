87bd881f5aaf6c6b86a10ac9b173a5a758b6efcc
Julius Bendezu <julius.bendezu@gmail.com>
2021-02-15 16:42:13 +0100

    More info in log

--- commands/handler.js
+++ commands/handler.js
@@ -2,14 +2,14 @@ const { CLIENT_ID } = require('../config');
 const { commandMatcher: bunkerMatcher, handleCommand: bunker } = require('./bunker/command');
 
 const commands = [
-	[bunkerMatcher, bunker]
+	[bunkerMatcher, bunker, 'Bunker']
 ];
 
 const runCommand = (command, args, msg) => {
 	console.log('Command to match:', command);
-	for (const [regex, func] of commands) {
+	for (const [regex, func, name] of commands) {
 		if (regex.test(command)) {
-			console.log(`"${command}" matched to "${func.name}" command with "${regex}"`);
+			console.log(`"${command}" matched to "${name}" command with "${regex}"`);
 			return func(msg, args); //Run first command that matches passed command
 		}
 	}
