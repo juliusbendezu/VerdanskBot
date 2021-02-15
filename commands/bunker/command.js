e8493b713a861d0581e7212c346ee14fbaa2a533
Julius Bendezu <julius.bendezu@gmail.com>
2021-02-15 16:40:37 +0100

    Add logic for only showing full map when no/'all' args passed

--- commands/bunker/command.js
+++ commands/bunker/command.js
@@ -13,10 +13,6 @@ const getLocationInfo = (location) => {
 			locationInfo.push([k, v]);
 		}
 	}
-	if (locationInfo.length == 0) { //If no match is found, be kind and show all instead of nothing
-		locationInfo.push([location + '?\n', {}]); //Not found location is sent back for display if desired
-		locationInfo.push(...getLocationInfo('all'));
-	}
 	return locationInfo;
 }
 
@@ -25,23 +21,34 @@ const commandMatcher = /^bunk(er)?s?(codes?)?$/;
 const handleCommand = (msg, args) => {
 	const location = args ? args.join(' ') : null;
 
-	//TODO: (maybe) only send when all locations are sent, otherwise send for matched locations
-	const bunkerLocationsImg = 'https://imgur.com/B9Pp9lI';
-	const bunkerCodesImg = 'https://imgur.com/a/yY6Nka8';
-	msg.channel.send(bunkerLocationsImg);
-	msg.channel.send(bunkerCodesImg);
-
+	let showFullMap = location == 'all' || !location;
 	const locationInfo = getLocationInfo(location);
-	console.log(locationInfo);
-	const infoString = locationInfo.reduce((acc, [name, loc]) => {
-		acc += name.charAt(0).toUpperCase() + name.substring(1);
-		if (loc.code) {
-			acc += ` Code: ${loc.code}`
+	if (locationInfo.length == 0) { //If no match is found, be kind and show all instead of nothing
+		locationInfo = getLocationInfo('all');
+		showFullMap = true;
+	}
+
+	if (showFullMap) {
+		const bunkerLocationsImg = 'https://imgur.com/B9Pp9lI';
+		const bunkerCodesImg = 'https://imgur.com/a/yY6Nka8';
+		msg.channel.send(bunkerLocationsImg);
+		msg.channel.send(bunkerCodesImg);
+	}
+
+	const info = locationInfo.reduce((acc, [name, loc], i) => {
+		name = (name[0].toUpperCase() + name.substring(1)).replace('_', ' ');
+		console.log(`Location ${i}:`, [name, loc]);
+
+		if (!showFullMap) {
+			acc += loc.img + '\n';
 		}
-		acc += '\n';
+
+		const code = loc.code ? loc.code : 'X';
+		acc += `${name} - Code: ${code}\n`;
 		return acc;
 	}, '');
-	msg.channel.send(infoString);
+
+	msg.channel.send(info);
 }
 
 module.exports = {
