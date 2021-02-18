33967614e2714619554ab6e38145249c9a051e24
Julius Bendezu <julius.bendezu@gmail.com>
2021-02-18 09:27:14 +0100

    Get help when @ the bot

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ const client = new Discord.Client();
 const commandHandler = require('./commands/handler');
 
 client.on('ready', () => {
-    console.log('VerdanskBot at your service');
+    console.log(`${client.user.tag} at your service`);
 });
 
 client.on('message', (msg) => {
+    if (msg.mentions.users.get(client.user.id)) { //If someones @ the bot
+        msg.content = '!help';
+        msg.reply('Привет');
+    }
     commandHandler(msg);
 });
 
